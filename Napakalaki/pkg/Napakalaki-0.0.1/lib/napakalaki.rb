7fb8f41d24157222867a7ae4c74b5238c2fa766f
Cristian V. <universidadcvr@gmail.com>
2015-11-09 17:01:12 +0100

    Cartas Implementadas

--- Napakalaki/pkg/Napakalaki-0.0.1/lib/napakalaki.rb
+++ Napakalaki/pkg/Napakalaki-0.0.1/lib/napakalaki.rb
@@ -1,6 +1,8 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+require_relative "card_dealer"
+require_relative "player"
 require 'singleton'
 
 class Napakalaki
