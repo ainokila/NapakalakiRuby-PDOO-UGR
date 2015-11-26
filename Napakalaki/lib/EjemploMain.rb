ddfe41f2a0fd23bfba03603edced652d14eb1505
Cristian V. <universidadcvr@gmail.com>
2015-11-26 16:05:54 +0100

    Empezando con ruby

--- Napakalaki/lib/EjemploMain.rb
+++ Napakalaki/lib/EjemploMain.rb
@@ -2,8 +2,8 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-require_relative "Napakalaki"
-require_relative "GameTester"
+require_relative "napakalaki.rb"
+require_relative "GameTester.rb"
 
 module NapakalakiGame
 
